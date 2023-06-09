bc6d9593e67f3950451b3522f23d4e04ca4fb217
admin <krivalcevich@gmail.com>
2023-06-09 19:55:40 +0300

    added avatar block for mobile

--- src/Pages/avatar/Avatar.jsx
+++ src/Pages/avatar/Avatar.jsx
@@ -1,10 +1,39 @@
 import React from 'react';
+import style from './avatar.module.scss'
+import {ReactComponent as TelegramIcon} from "../../assets/pictures/svg/telegram/Telegram Messenger.svg";
+import {ReactComponent as MailIcon} from "../../assets/pictures/svg/mail/Vector.svg";
+import avatar from '../../assets/pictures/Photos/avatar.png'
+import bigAvatar from '../../assets/pictures/Photos/BigAvatar.png'
+import circleText from '../../assets/pictures/png/круговая подпись.png'
 
 export const Avatar = () => {
     return (
-       <section>
+        <section className={style.avatar}>
+            <div className={style.avatarDesktop}>
+                <div className={style.heading}>
+                    <h1 className={style.heading__title}>ALEX YUNCHITS </h1>
+                    <div>
+                        <TelegramIcon className={style.telegram}/>
+                        <MailIcon/>
+                    </div>
 
-       </section>
+                </div>
+                <p className={style.text}>Привет! Меня зовут Алексей Юнчиц.
+                    Я Full Stack разработчик и преподаватель. </p>
+            </div>
+            <div className={style.mainPhoto}>
+                <img src={avatar} alt="main photo"/>
+                <div className={style.circle}>
+                    <img src={circleText} alt="circle"/>
+                </div>
+            </div>
+            <div className={style.mainBigPhoto}>
+                <img src={bigAvatar} alt="main photo"/>
+                <div className={style.circle}>
+                    <img src={circleText} alt="circle"/>
+                </div>
+            </div>
+        </section>
     );
 };
 
