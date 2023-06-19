4faa99529bbe31078de216a562d075d07d0e37fe
admin <krivalcevich@gmail.com>
2023-06-19 16:06:22 +0300

    bugFix with modal

--- src/Modal/Modal.jsx
+++ src/Modal/Modal.jsx
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import style from './modal.module.scss'
 
 export const Modal = ({children}) => {
+    useEffect(() => {
+        const scrollTop = window.scrollY
+        const bodyNode = document.querySelector('body')
+        if (bodyNode) {
+            bodyNode.style.position = 'fixed'
+            bodyNode.style.top = -scrollTop + 'px'
+            bodyNode.style.left = '0px'
+            bodyNode.style.right = '0px'
+        }
+        return () => {
+            if (bodyNode) {
+                bodyNode.style.removeProperty('top')
+                bodyNode.style.removeProperty('left')
+                bodyNode.style.removeProperty('right')
+                bodyNode.style.removeProperty('position')
+                window.scrollTo(0, scrollTop)
+            }
+        }
+    }, [])
     return (
         <div className={style.modal}>
             <div className={style.modal__content}>
