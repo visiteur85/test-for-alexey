13058d04acb22c8ee307c060251cef62314e26bc
admin <krivalcevich@gmail.com>
2023-06-25 17:24:22 +0300

    separated components

--- src/Pages/videoLessons/VideoLessons.jsx
+++ src/Pages/videoLessons/VideoLessons.jsx
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import style from './videoLessons.module.scss'
 import {Title} from "../../Components/title/Title";
 import {videoLessons} from "../../data/data";
+import {Technology} from "./videoLesson/Technology";
 
 export const VideoLessons = () => {
     const [chosenTech, setChosenTech] = useState(null);
@@ -17,7 +18,7 @@ export const VideoLessons = () => {
             const updatedTech = {
                 ...chosenTech,
                 videoLessonsList: chosenTech.videoLessonsList.map((lesson) => {
-                    return lesson.id === id ? { ...lesson, isShown: true } : { ...lesson, isShown: false };
+                    return lesson.id === id ? {...lesson, isShown: true} : {...lesson, isShown: false};
                 })
             };
             setChosenTech(updatedTech);
@@ -45,27 +46,14 @@ export const VideoLessons = () => {
                 </div>
                 {chosenTech && <h3>Уроки по {chosenTech.title}</h3>}
                 {chosenTech && chosenTech.videoLessonsList.map(({id, name, url, isShown}) =>
-                    <div className={style.oneTech} key={id}>{<Technology  id={id} name={name} isShown={isShown} url={url} callback={()=>{changeActiveLesson(id)}} />}</div>)}
+                    <div className={style.oneTech} key={id}>{<Technology id={id} name={name} isShown={isShown} url={url}
+                                                                         callback={() => {
+                                                                             changeActiveLesson(id)
+                                                                         }}/>}</div>)}
             </div>
         </section>
     );
 };
 
-export const Technology = ({id, name, url, isShown, callback}) => {
 
-    const changeActiveVideo = ()=> {
-        callback(id)
-    }
-    return (
-        <div className={style.technology}>
-            <div onClick={changeActiveVideo} className={`${style.lesson} ${isShown ? style.active : ''}`}>{name}</div>
-            {isShown && <div className={style.frame}>
-                <iframe src={url} title="YouTube video player"
-                        allow="accelerometer; autoplay;  gyroscope; picture-in-picture; web-share"
-                        allowFullScreen="" width="100%" height="100%" frameBorder="0"></iframe>
-            </div>}
-
-        </div>
-    )
-}
 
