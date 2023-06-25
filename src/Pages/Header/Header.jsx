01417255de6f2623e93a663d0c9b9c678f346589
admin <krivalcevich@gmail.com>
2023-06-25 17:50:43 +0300

    added icon return to main page

--- src/Pages/Header/Header.jsx
+++ src/Pages/Header/Header.jsx
@@ -5,17 +5,24 @@ import {ReactComponent as BurgerButton} from '../../assets/pictures/svg/burger/G
 import {ReactComponent as CloseButton} from '../../assets/pictures/svg/closeButton/Выйти CTA.svg';
 import {NavMenu} from "../../Components/navMenu/NavMenu";
 import {menuList} from "../../data/data";
+import {returnIcon} from "../../assets/icons/return";
+import {useLocation, useNavigate} from "react-router-dom";
 
 
 export const Header = () => {
     const [isNavMenuOpen, setNavMenuOpen] = useState(false);
+    const location = useLocation().pathname
+    const navigate = useNavigate()
 
 
     const onClickMenuChange = () => {
         setNavMenuOpen(prevState => !prevState)
 
     }
-//check
+
+    const returnToMainPage = ()=> {
+        navigate('/')
+    }
     const isOpenMenu = isNavMenuOpen ? style.open : '';
     const isVisibleBurger = isNavMenuOpen ? style.close : "";
     const isVisibleClosedButton = !isNavMenuOpen ? style.close : "";
@@ -26,34 +33,44 @@ export const Header = () => {
         <section className={style.header}>
 
             <div className='container'>
-            <div className={style.header__inner}>
-                <div className={style.logo}>
-                    <img className={style.logo} src={logo} alt="logo"/>
-                </div>
-                <div className={style.navMenu}>
-                    <div className={style.navMenuMobile}>
-                        <div className={`${isVisibleClosedButton} `}>
-                            <CloseButton className={style.closeButton}
-                                         onClick={onClickMenuChange}/>
+                <div className={style.header__inner}>
+                    <div className={style.logo}>
+                        <img className={style.logo} src={logo} alt="logo"/>
+                    </div>
+                    {location === '/videoLessons' ?
+                        <div className={style.returnMainPage} onClick={returnToMainPage}>
+                        <img width='20' height='20' src={`data:image/svg+xml,${encodeURIComponent(returnIcon)}`}
+                             alt=""/>
+                         На главную
                         </div>
-                        <div className={`${style.navMenuMobile__menu} ${isOpenMenu}`}>
-                            <NavMenu callback={onClickMenuChange}
-                                     menuList={menuList}/></div>
-                    </div>
-                    <div className={`${isVisibleBurger} `}>
-                        <BurgerButton onClick={onClickMenuChange} className={style.burgerButton}/>
-                    </div>
+                        : <>
+                            <div className={style.navMenu}>
+                                <div className={style.navMenuMobile}>
+                                    <div className={`${isVisibleClosedButton} `}>
+                                        <CloseButton className={style.closeButton}
+                                                     onClick={onClickMenuChange}/>
+                                    </div>
+                                    <div className={`${style.navMenuMobile__menu} ${isOpenMenu}`}>
+                                        <NavMenu callback={onClickMenuChange}
+                                                 menuList={menuList}/></div>
+                                </div>
+                                <div className={`${isVisibleBurger} `}>
+                                    <BurgerButton onClick={onClickMenuChange} className={style.burgerButton}/>
+                                </div>
+
+                            </div>
+                            <div className={style.desktopMenu}>
+                                <NavMenu menuList={menuList} callback={onClickMenuChange}/>
+                            </div>
+                        </>}
+
 
                 </div>
-                <div className={style.desktopMenu}>
-                    <NavMenu menuList={menuList} callback={onClickMenuChange}/>
-                </div>
-
-            </div>
             </div>
         </section>
 
     );
 };
+
 
 
