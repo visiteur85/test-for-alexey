a0799b066d6b84b9e73b7d3b789985dc782c678a
admin <krivalcevich@gmail.com>
2023-06-26 12:05:09 +0300

    fixed bug with about me and avatar

--- src/Components/NavMenu/NavMenu.jsx
+++ src/Components/NavMenu/NavMenu.jsx
@@ -11,8 +11,8 @@ export const NavMenu = ({menuList, callback, block}) => {
             <ul className={`${block === 'footer' ? style.blockFooter : style.list}`}>
                 {menuList.map(({id, path, name, isRoute}) => (
                     <li className={`${block === 'footer' ? style.blockFooter__item : style.list__item}`} key={id}>
-                        {isRoute ? <NavLink onClick={callback} to={'/videoLessons'}>{name}</NavLink> :
-                            <Link onClick={callback} offset={-45} smooth={true} duration={500} to={path}>{name} </Link>
+                        {isRoute ? <NavLink onClick={()=>callback(false)} to={'/videoLessons'}>{name}</NavLink> :
+                            <Link onClick={()=>callback(false)} offset={-45} smooth={true} duration={500} to={path}>{name} </Link>
                         }
                     </li>
                 ))}
