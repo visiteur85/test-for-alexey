2354eaaaea792dc230c9b37a5dc95448140a51ed
admin <krivalcevich@gmail.com>
2023-06-28 12:41:36 +0300

    second nav menu

--- src/Components/NavMenu/NavMenu.jsx
+++ src/Components/NavMenu/NavMenu.jsx
@@ -5,14 +5,18 @@ import {NavLink} from "react-router-dom";
 
 export const NavMenu = ({menuList, callback, block}) => {
 
+    const routingAndChangeNavMenu  = (path)=> {
+        callback(path)
+    };
+
 
     return (
         <nav>
             <ul className={`${block === 'footer' ? style.blockFooter : style.list}`}>
                 {menuList.map(({id, path, name, isRoute}) => (
                     <li className={`${block === 'footer' ? style.blockFooter__item : style.list__item}`} key={id}>
-                        {isRoute ? <NavLink onClick={()=>callback(false)} to={'/videoLessons'}>{name}</NavLink> :
-                            <Link onClick={()=>callback(false)} offset={-45} smooth={true} duration={500} to={path}>{name} </Link>
+                        {isRoute ? <NavLink onClick={routingAndChangeNavMenu} to={'/videoLessons'}>{name}</NavLink> :
+                            <Link onClick={()=>routingAndChangeNavMenu(path)} offset={-45} smooth={true} duration={500} to={path}>{name} </Link>
                         }
                     </li>
                 ))}
